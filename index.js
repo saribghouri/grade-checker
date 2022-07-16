9a0889bef9a94f6fd0bbdb18fb594ddba4e18aa9
saribghouri <sarib.ghouri92@gmail.com>
2022-07-16 08:02:43 -0700

    complete grade checker

---
+++ index.js
@@ -0,0 +1,60 @@
+const calculate = () => {
+  // Getting input from user into height variable.
+  let chemistry = document.querySelector("#chemistry").value;
+  let urdu = document.querySelector("#urdu").value;
+  let maths = document.querySelector("#maths").value;
+  let english = document.querySelector("#english").value;
+  let pakStudies = document.querySelector("#pak-studies").value;
+  let phy = document.querySelector("#phy").value;
+  let grades = "";
+
+  // Input is string so typecasting is necessary. */
+  let totalgrades =
+    parseFloat(chemistry) +
+    parseFloat(urdu) +
+    parseFloat(maths) +
+    parseFloat(english) +
+    parseFloat(pakStudies) +
+    parseFloat(phy);
+
+  // Checking the condition for the providing the
+  // grade to student based on percentage
+  let percentage = (totalgrades / 600) * 100;
+  if (percentage <= 100 && percentage >= 80) {
+    grades = "A";
+  } else if (percentage <= 79 && percentage >= 60) {
+    grades = "B";
+  } else if (percentage <= 59 && percentage >= 40) {
+    grades = "C";
+  } else {
+    grades = "F";
+  }
+  // Checking the values are empty if empty than
+  // show please fill them
+  if (
+    chemistry == "" ||
+    urdu == "" ||
+    maths == "" ||
+    english == "" ||
+    pakStudies == "" ||
+    phy == ""
+  ) {
+    document.querySelector("#showdata").innerHTML =
+      "Please enter all the fields";
+  } else {
+    // Checking the condition for the fail and pass
+    if (percentage >= 39.5) {
+      document.querySelector(
+        "#showdata"
+      ).innerHTML = ` Out of 600 your total is  ${totalgrades} 
+    and percentage is ${percentage}%. <br> 
+    Your grade is ${grades}. You are Pass. `;
+    } else {
+      document.querySelector(
+        "#showdata"
+      ).innerHTML = ` Out of 400 your total is  ${totalgrades} 
+    and percentage is ${percentage}%. <br> 
+    Your grade is ${grades}. You are Fail. `;
+    }
+  }
+};
